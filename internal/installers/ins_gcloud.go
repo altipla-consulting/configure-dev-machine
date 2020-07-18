df76b12f1822cb4488879ba3bb6a34c453b25322
Ernesto Alejo <ernestokarim@gmail.com>
2020-07-18 22:52:31 +0200

    Revert "Fuerza una versión concreta de gcloud mientras solucionan el problema."
    This reverts commit 155e4d0665fa255f6ef16a52f5d82462725c5fcf.

--- internal/installers/ins_gcloud.go
+++ internal/installers/ins_gcloud.go
@@ -23,7 +23,7 @@ func (ins *insGcloud) Install() error {
     echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list
     curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
     sudo apt update
-    sudo apt install -y google-cloud-sdk=297.0.1-0 kubectl
+    sudo apt install -y google-cloud-sdk kubectl
   `
 	if err := run.Shell(script); err != nil {
 		return errors.Trace(err)
