c32faeb250afc8a67d80057146350e4e6f5bb57a
Ernesto Alejo <ernestokarim@gmail.com>
2020-07-18 16:37:57 +0200

    Crea la carpeta de binarios si no existe al instalar CI.
    Algunas máquinas como Debian o Jenkins no lo tienen.

--- configure.sh
+++ configure.sh
@@ -15,6 +15,7 @@ install_apt
 # ------------------------------------------------------------------------------
  
 function install_ci {
+  mkdir -p ~/bin
   curl -L -o ~/bin/ci $(curl --silent 'https://api.github.com/repos/altipla-consulting/ci/releases/latest' | jq -r '.assets[0].browser_download_url') && chmod +x ~/bin/ci
 }
 
