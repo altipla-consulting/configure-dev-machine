2830ba4aecf0f6c3da4d599805b16b850a79767a
Ernesto Alejo <ernestokarim@gmail.com>
2020-07-18 22:15:52 +0200

    Arregla un typo.

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ set -eu
 
 sudo apt install -y jq
 
-curl -L -o ~/bin/configure-dev-machine $(curl --silent 'https://api.github.com/repos/altipla-consulting/reloader/releases/latest' | jq -r '.assets[0].browser_download_url')
+curl -L -o ~/bin/configure-dev-machine $(curl --silent 'https://api.github.com/repos/altipla-consulting/configure-dev-machine/releases/latest' | jq -r '.assets[0].browser_download_url')
 chmod +x ~/bin/configure-dev-machine
 
 ~/bin/configure-dev-machine
