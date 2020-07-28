9491d5d7af813fd284e0ba1fe45d268632e338b6
David O <dvormagic@gmail.com>
2020-07-28 09:16:07 +0200

    Prepara entorno para zshell. (#3)

--- internal/installers/ins_mkcert.go
+++ internal/installers/ins_mkcert.go
@@ -1,26 +1,26 @@
 package installers
 
 import (
-  "libs.altipla.consulting/errors"
+	"libs.altipla.consulting/errors"
 
-  "github.com/altipla-consulting/configure-dev-machine/internal/run"
+	"github.com/altipla-consulting/configure-dev-machine/internal/run"
 )
 
 type insMkcert struct{}
 
 func (ins *insMkcert) Name() string {
-  return "mkcert"
+	return "mkcert"
 }
 
 func (ins *insMkcert) Check() (*CheckResult, error) {
-  return NeedsInstall, nil
+	return NeedsInstall, nil
 }
 
 func (ins *insMkcert) Install() error {
-  script := `
+	script := `
     curl -L -o ~/bin/mkcert https://github.com/FiloSottile/mkcert/releases/download/v1.4.1/mkcert-v1.4.1-linux-amd64
     chmod +x ~/bin/mkcert
     mkcert -install
   `
-  return errors.Trace(run.Shell(script))
+	return errors.Trace(run.Shell(script))
 }
