e8aab747a95e1158704e651887952c2b00ab6ec7
Ernesto Alejo <ernestokarim@gmail.com>
2020-09-15 21:01:41 +0200

    Corrige la sintaxis para grabar permanentemente las configuraciones.

--- internal/installers/ins_enospc.go
+++ internal/installers/ins_enospc.go
@@ -1,6 +1,9 @@
 package installers
 
 import (
+	"io/ioutil"
+	"strings"
+
 	"libs.altipla.consulting/errors"
 
 	"github.com/altipla-consulting/configure-dev-machine/internal/run"
@@ -13,12 +16,21 @@ func (ins *insEnospc) Name() string {
 }
 
 func (ins *insEnospc) Check() (*CheckResult, error) {
+	content, err := ioutil.ReadFile("/etc/sysctl.conf")
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+	for _, line := range strings.Split(string(content), "\n") {
+		if strings.HasPrefix(line, "fs.inotify.max_user_watches") {
+			return nil, nil
+		}
+	}
 	return NeedsInstall, nil
 }
 
 func (ins *insEnospc) Install() error {
 	script := `
-		sudo sysctl -w fs.inotify.max_user_watches=524288
+		echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
 		sudo sysctl --system
   `
 	return errors.Trace(run.Shell(script))
--- internal/installers/ins_ipv4_forwarding.go
+++ internal/installers/ins_ipv4_forwarding.go
@@ -1,6 +1,9 @@
 package installers
 
 import (
+	"io/ioutil"
+	"strings"
+
 	"libs.altipla.consulting/errors"
 
 	"github.com/altipla-consulting/configure-dev-machine/internal/run"
@@ -13,12 +16,21 @@ func (ins *insIPV4Forwarding) Name() string {
 }
 
 func (ins *insIPV4Forwarding) Check() (*CheckResult, error) {
+	content, err := ioutil.ReadFile("/etc/sysctl.conf")
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+	for _, line := range strings.Split(string(content), "\n") {
+		if strings.HasPrefix(line, "net.ipv4.ip_forward") {
+			return nil, nil
+		}
+	}
 	return NeedsInstall, nil
 }
 
 func (ins *insIPV4Forwarding) Install() error {
 	script := `
-		sudo sysctl -w net.ipv4.ip_forward=1
+		echo net.ipv4.ip_forward=1 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
 		sudo sysctl --system
   `
 	return errors.Trace(run.Shell(script))
