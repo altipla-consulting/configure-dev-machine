9e1cd07aa29507da2f14b2af1daa83400044b39c
Ernesto Alejo <ernestokarim@gmail.com>
2020-11-15 03:37:38 +0100

    Ordena el despliegue a producción para que llegue primero el nuevo binario.
    Change-Id: I838b8123dd68c5dfcc5039bfd909d612ba6eec0b

--- infra/ci/deploy.sh
+++ infra/ci/deploy.sh
@@ -12,9 +12,9 @@ mkdir -p tmp
 run "go build -o tmp/configure-dev-machine -ldflags=\"-X 'main.Version=$(build-tag)'\" ./cmd/configure-dev-machine"
 run "gsutil -h 'Cache-Control: no-cache' cp tmp/configure-dev-machine gs://tools.altipla.consulting/bin/configure-dev-machine"
 
+run "gsutil -h 'Cache-Control: no-cache' cp infra/install.sh gs://tools.altipla.consulting/install/configure-dev-machine"
+
 run "echo $(build-tag) > tmp/version"
 run "gsutil -h 'Cache-Control: no-cache' cp tmp/version gs://tools.altipla.consulting/version-manifest/configure-dev-machine"
-
-run "gsutil -h 'Cache-Control: no-cache' cp infra/install.sh gs://tools.altipla.consulting/install/configure-dev-machine"
 
 git-tag
