ccedf87a8a1c51bd6307a0299f62ff8f8b05632c
Ernesto Alejo <ernestokarim@gmail.com>
2020-11-15 03:28:05 +0100

    Instala Azure CLI manualmente para evitar el error en Linux Mint.
    El nombre de la release no se extraía adecuadamente y fallaba por tanto.
    
    Change-Id: Ic205d8a93cdef1e9d0ef2530fafcd2e7bc8d377d

--- internal/installers/ins_az.go
+++ internal/installers/ins_az.go
@@ -18,7 +18,14 @@ func (ins *insAZ) Check() (*CheckResult, error) {
 
 func (ins *insAZ) Install() error {
 	script := `
-    curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+		source /etc/os-release
+
+		sudo apt-get update
+		sudo apt-get install ca-certificates curl apt-transport-https lsb-release gnupg
+		curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
+		echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $UBUNTU_CODENAME main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
+		sudo apt-get update
+		sudo apt-get install azure-cli
   `
 	if err := run.Shell(script); err != nil {
 		return errors.Trace(err)
