d73135e5a7a8a45a048880a896989ed6bc4b71a2
Alberto Moreno <albertinivva@gmail.com>
2021-02-03 11:03:10 +0100

    Instala los paquetes de npm por separado.
    Para evitar errores.
    
    Change-Id: Iad4420ea971b4c7ad4cc3e090608d99574721560

--- internal/installers/ins_npmpackages.go
+++ internal/installers/ins_npmpackages.go
@@ -18,7 +18,9 @@ func (ins *insNpmpackages) Check() (*CheckResult, error) {
 
 func (ins *insNpmpackages) Install() error {
 	script := `
-	sudo npm install -g npm@latest yarn@latest netlify-cli@latest
+	sudo npm install -g npm@latest
+	sudo npm install -g yarn@latest
+	sudo npm install -g netlify-cli@latest
   `
 	return errors.Trace(run.Shell(script))
 }
